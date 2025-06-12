b09cdb91c26b17801299a6f00aaeaa20c26fd54f
mukisa-emmanuel <mukisae44@gmail.com>
2025-06-12 21:14:18 +0300

    Add files via upload

---
+++ bsit2.sql
@@ -0,0 +1,15 @@
+create database bsit2;
+use bsit2;
+create table students(
+id INT PRIMARY KEY,
+first_name VARCHAR (25),
+last_name VARCHAR (20),
+age INT);
+DESC students;
+show tables;
+SELECT * FROM students;
+INSERT INTO students VALUES(4,"Kelly","Mosha",20);
+
+
+
+
